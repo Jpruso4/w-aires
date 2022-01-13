c508c08f403d420130115014fe375233ea94be04
Juan Diego <juan_grisales23201@elpoli.edu.co>
2022-01-12 21:50:00 -0500

    Backend inicial

---
+++ Backend/W-aires/W-aires/src/main/java/com/waires/Waires/WAiresApplication.java
@@ -0,0 +1,13 @@
+package com.waires.Waires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WAiresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WAiresApplication.class, args);
+	}
+
+}
